bf6f400676a300e7f97c8470ac068180d73ff664
Romchirik <r.titov@g.nsu.ru>
2021-11-03 14:11:05 +0700

    started lab

---
+++ AsyncCommunicationLab3/src/main/java/nsu/titov/Main.java
@@ -0,0 +1,10 @@
+package nsu.titov;
+
+import javafx.application.Application;
+import nsu.titov.app.App;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
---
+++ AsyncCommunicationLab3/src/main/java/nsu/titov/app/App.java
@@ -0,0 +1,11 @@
+package nsu.titov.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
