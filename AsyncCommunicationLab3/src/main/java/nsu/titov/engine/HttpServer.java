5ddbedb0f801a538909b68c1c37896aaff6ff8a5
Romchirik <r.titov@g.nsu.ru>
2021-11-15 17:27:46 +0700

    code cleanup

--- AsyncCommunicationLab3/src/main/java/nsu/titov/engine/HttpServer.java
+++ AsyncCommunicationLab3/src/main/java/nsu/titov/engine/HttpServer.java
@@ -1,6 +1,5 @@
 package nsu.titov.engine;
 
-import lombok.SneakyThrows;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.asynchttpclient.AsyncHttpClient;
